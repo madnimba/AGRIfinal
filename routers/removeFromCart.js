ca3ab765b03807bbdaab0a632a83d9f3fe2932de
madnimba <jarintasneem248@gmail.com>
2023-11-20 19:53:12 +0600

    addtocart

--- routers/removeFromCart.js
+++ routers/removeFromCart.js
@@ -8,8 +8,11 @@ router.post('/',async(req,res)=>{
     
     try{
         const prod = global.cart.find((object) => object.id === productId);
+        const product = global.products.find((object) => object.id ===productId)
+        product.incart = '0';
 
         const index = global.cart.indexOf(prod);
+        
         
         global.cart.splice(index, 1);
         console.log(global.cart);
