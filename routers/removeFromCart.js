b48caba4d6fdeaddae38b030d72eb528950dbf6b
madnimba <jarintasneem248@gmail.com>
2023-11-20 19:30:12 +0600

    initialize

---
+++ routers/removeFromCart.js
@@ -0,0 +1,27 @@
+const express=require('express');
+
+const router=express.Router();
+
+router.post('/',async(req,res)=>{
+    var productId = req.body.productId;
+    
+    
+    try{
+        const prod = global.cart.find((object) => object.id === productId);
+
+        const index = global.cart.indexOf(prod);
+        
+        global.cart.splice(index, 1);
+        console.log(global.cart);
+    res.sendStatus(200);
+    }catch(err)
+    {
+        res.status(404);
+    }
+    
+})
+
+
+
+
+module.exports= router;
